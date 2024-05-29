765113f8268a8b4bb46b38a75547ce18e320116a
Marcos Di Caprio <45523490+dicapriomarcos@users.noreply.github.com>
2024-05-30 00:37:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,3 +52,62 @@ app.get('/api/persons', (request, response) => {
     console.log(`Error:`, e)
   })
 })
+
+
+
+
+app.post('/api/persons', ( request, response ) => {
+
+    let errorMessages = [];
+
+    const body = request.body
+
+    const name = body.name
+
+    // name validation
+    if( !name ){
+        errorMessages.push('name don´t be empty')
+    }else{
+
+        const duplicateName = persons.filter( person => person.name === name )
+        
+        if( duplicateName.length > 0 ){
+            errorMessages.push(`name ${name} exist in Phonebook`)
+        } 
+
+    }
+
+    const number = body.number
+
+    // number validation
+    if( !number ){
+        errorMessages.push('number don´t be empty')
+    }
+    
+    if( errorMessages.length > 0 ){
+
+        const errorMessage = errorMessages.map(error => `<p>${error}<p>`).join('<br>')
+        response.send(errorMessage).status(403).end()
+
+    }else{
+
+        const personObj = {
+            id: Math.floor(Math.random() * 9999),
+            name: name,
+            number: number
+
+        }
+
+        persons = [...persons, personObj]
+
+        response.send(`New contact ${name} with number ${number} was added to Phonebook`).status(200).end()
+    }
+    
+    
+
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
